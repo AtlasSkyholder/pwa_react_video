0c9e56c9c4db19175e7f53d4ef7399abf621e268
AtlasSkyholder <thecanuckatheist@gmail.com>
2022-12-12 13:10:32 -0500

    exported fetchWeather, imported it to App.js

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = process.env.API_PASS;
 
-const fetchWeather = async (query) => {
-    const response = await axios.get(URL);
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
 }
