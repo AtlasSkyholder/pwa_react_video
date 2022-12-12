4aba5f7a383e85c7d6c82139d0673f5041f1209e
AtlasSkyholder <thecanuckatheist@gmail.com>
2022-12-12 13:04:26 -0500

    added dotenv for protecting api password

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = process.env.API_PASS;
 
 const fetchWeather = async (query) => {
     const response = await axios.get(URL);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const dotenv = require('dotenv');
 import App from './App';
+
+dotenv.config({path: "./config.env" });
 
 ReactDOM.render(<App />, document.getElementById('root'));
